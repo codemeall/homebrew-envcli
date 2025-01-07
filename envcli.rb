554f5c85670817d12e0250dc5be2138ff6258bc3
Abdul Aziz <aziz@marensolutions.com>
2025-01-07 23:57:10 +0530

    test 5

--- envcli.rb
+++ envcli.rb
@@ -4,44 +4,15 @@ class Envcli < Formula
   url "https://github.com/codemeall/irootcli/releases/download/v1.0.1/envcli-1.0.7.tgz"
   sha256 "3c72acfcf3578c38b73234758cbd779ec9345ffe5eb8412f1de96f51c55ea824"
   license "ISC"
+
   livecheck do
     url :stable
     strategy :github_latest
   end
-  # Remove direct node dependency
-  # depends_on "node"
+
+  depends_on "node"
+
   def install
-    # Check for existing node installation in multiple locations
-    node_path = nil
-    
-    # Check in Homebrew
-    brew_node = "/usr/local/bin/node"
-    if File.exist?(brew_node)
-      node_path = brew_node
-    end
-    
-    # Check in system bin folder
-    bin_node = "/usr/bin/node"
-    if node_path.nil? && File.exist?(bin_node)
-      node_path = bin_node
-    end
-    
-    # Check in PATH as fallback
-    if node_path.nil? && ENV["PATH"].split(File::PATH_SEPARATOR).any? { |path| File.exist?(File.join(path, "node")) }
-      node_path = `which node`.chomp
-    end
-    
-    # Verify we found node
-    if node_path.nil?
-      odie "Node.js is required but not found. Please install Node.js first."
-    end
-    
-    # Verify minimum node version
-    node_version = `"#{node_path}" --version`.chomp.delete_prefix("v")
-    if Gem::Version.new(node_version) < Gem::Version.new("14.0.0")
-      odie "Node.js version 14.0.0 or higher is required. Current version: #{node_version}"
-    end
-    
     # Extract the package contents
     system "tar", "xf", cached_download, "-C", buildpath
     
@@ -57,11 +28,11 @@ class Envcli < Formula
     libexec.install Dir["*"]
     libexec.install Dir["node_modules"]
     
-    # Create bin stubs using system node
+    # Create bin stubs
     (bin/"envcli").write <<~EOS
       #!/bin/bash
       export NODE_PATH="#{libexec}/node_modules"
-      exec "#{node_path}" "#{libexec}/index.js" "$@"
+      exec "#{Formula["node"].opt_bin}/node" "#{libexec}/index.js" "$@"
     EOS
     
     # Make the bin stub executable

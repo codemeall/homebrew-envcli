b9b319b6da59279b9a06f6e40fc8722e8bee019f
Abdul Aziz <aziz@marensolutions.com>
2025-01-07 14:14:46 +0530

    v1.0.3

--- envcli.rb
+++ envcli.rb
@@ -1,7 +1,7 @@
 class Envcli < Formula
     desc "A CLI tool for managing environment variables"
     homepage "https://github.com/codemeall/envcli"
-    url "https://github.com/codemeall/envcli/releases/download/v1.0.1/envcli-1.0.1.tgz"
+    url "https://github.com/codemeall/envcli/releases/download/v1.0.3/envcli-1.0.3.tgz"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     license "ISC"
   

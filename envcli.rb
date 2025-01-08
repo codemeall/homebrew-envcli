7c7734c3efc94ed76c42651a50ca319acbebf65a
Abdul Aziz <aziz@marensolutions.com>
2025-01-08 14:09:56 +0530

    obfuscated

--- envcli.rb
+++ envcli.rb
@@ -2,7 +2,7 @@ class Envcli < Formula
   desc "A CLI tool for managing environment variables"
   homepage "https://github.com/codemeall/envcli"
   url "https://github.com/codemeall/envcli/releases/download/v1.0.9/envcli-1.0.9.tgz"
-  sha256 "75d2da9bf082138999cf6db76850f373034b3e25acd671cecc06b2d46e8f67b0"
+  sha256 "ff38d4bb9930b0ef34680998bb8fd089f236a1862377b52c12794f77ed64c1eb"
   license "ISC"
 
   livecheck do

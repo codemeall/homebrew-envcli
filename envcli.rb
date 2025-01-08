70720c9ae966d30975d20546d7a70347f83ed93d
Abdul Aziz <aziz@marensolutions.com>
2025-01-08 13:29:02 +0530

    updated formula

--- envcli.rb
+++ envcli.rb
@@ -2,7 +2,7 @@ class Envcli < Formula
   desc "A CLI tool for managing environment variables"
   homepage "https://github.com/codemeall/envcli"
   url "https://github.com/codemeall/envcli/releases/download/v1.0.8/envcli-1.0.8.tgz"
-  sha256 "9a2f4fde7827512836900a2c7c27d13c68c8f1b4fe4c35329b01f8d699d15e2f"
+  sha256 "a9d4b90f714b57bf4e07f0504cdef06f4f805849cc9e40b742ae982bf09e35f9"
   license "ISC"
 
   livecheck do
@@ -19,17 +19,25 @@ class Envcli < Formula
     # Move package contents to libexec
     libexec.install Dir["*"]
     
-    # Create bin stubs
+    # Create bin stubs with environment variables
     (bin/"envcli").write <<~EOS
       #!/bin/bash
-      exec "\#{Formula["node"].opt_bin}/node" "\#{libexec}/index.js" "$@"
+      export NODE_PATH="#{libexec}/node_modules"
+      export ENVCLI_CONFIG_DIR="#{var}/envcli"
+      exec "#{Formula["node"].opt_bin}/node" "#{libexec}/index.js" ""
     EOS
     
     # Make the bin stub executable
     chmod 0755, bin/"envcli"
   end
 
+  def post_install
+    # Ensure config directory has correct permissions
+    (var/"envcli").mkpath
+    chmod 0755, var/"envcli"
+  end
+
   test do
-    assert_match "envcli", shell_output("\#{bin}/envcli about")
+    assert_match "envcli", shell_output("#{bin}/envcli about")
   end
 end
